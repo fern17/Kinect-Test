e009ecea1c480e775edf8bbed93a47852b3039dc
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2012-03-22 12:20:19 -0300

    Borrada basura

--- 15_kinect_mesh/getPointIndex.cpp
+++
@@ -1,8 +0,0 @@
-
-
-#include <cv.h>
-//Returns the index of the point at position (i,j) of the matrix M.
-unsigned int getPointIndex(unsigned int i, unsigned int j, unsigned int cols){
-			return (i*cols)+j;
-}
-
